b1e1658da2aa04292991a81155324b18d076b651
Tyler <Tyler@DESKTOP-NFGM2ET>
2023-03-11 09:38:00 -0800

    Add project files.

---
+++ SRC/Asteroid.h
@@ -0,0 +1,17 @@
+#ifndef __ASTEROID_H__
+#define __ASTEROID_H__
+
+#include "GameObject.h"
+
+class Asteroid : public GameObject
+{
+public:
+	Asteroid(void);
+	~Asteroid(void);
+
+	bool CollisionTest(shared_ptr<GameObject> o);
+	void OnCollision(const GameObjectList& objects);
+	void SplitAsteroid(shared_ptr<GameObject> o);
+};
+
+#endif
